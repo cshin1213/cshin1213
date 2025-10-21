63190de5987bf038acd3d864c967c669ce3bcb8c
cshin1213 <cshin1213@gmail.com>
2025-10-21 11:05:38 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask
-
 app = Flask(__name__)
 
-@app.get("/")
-def index():
-    return "Hello from Render + Flask + Gunicorn!"
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
